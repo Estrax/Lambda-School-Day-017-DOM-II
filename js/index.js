926e247dc6964df4699784b88ee2c15ba085e452
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-01-29 15:34:48 -0500

    added task3 - dblclick

--- js/index.js
+++ js/index.js
@@ -1,9 +1,17 @@
 // Your code goes here
 
+// selectors
 const navItems = document.querySelectorAll('.nav-link');
+const home_p = document.querySelectorAll('p');
+
+// helper functions
 
 function randomizeColor(){
     return `rgb(${Math.random()*127+128}, ${Math.random()*255}, ${Math.random()*63+192})`;
+}
+
+function boldParagraphs(){
+    return Math.floor(Math.random()*6)*100+100;
 }
 
 // navbar items
@@ -12,3 +20,9 @@ navItems.forEach(elem => {
     elem.addEventListener('mouseover', e => e.target.style.color = randomizeColor());
 });
 
+home_p.forEach(elem => {
+    elem.addEventListener('dblclick', e => {
+        e.target.style.fontWeight = boldParagraphs();
+        e.target.style.color = randomizeColor();
+    });
+});
