7ad3038370e03aeb4809aff2158ed3c3c6a6280a
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-01-29 15:18:47 -0500

    added task3 - stop the navigation from items with preventDefault()

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+const navItems = document.querySelectorAll('.nav-link');
+
+// navbar items
+navItems.forEach(elem => {
+    elem.addEventListener('click', e => e.preventDefault());
+})
