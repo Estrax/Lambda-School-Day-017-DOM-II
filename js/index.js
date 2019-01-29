f5cab30d6c7c9a4dc2bfad1be11115b08efbcd33
Łukasz Siatka <lukasz@lukaszsiatka.pl>
2019-01-29 15:23:00 -0500

    added task3 - mouseover

--- js/index.js
+++ js/index.js
@@ -2,7 +2,13 @@
 
 const navItems = document.querySelectorAll('.nav-link');
 
+function randomizeColor(){
+    return `rgb(${Math.random()*127+128}, ${Math.random()*255}, ${Math.random()*63+192})`;
+}
+
 // navbar items
 navItems.forEach(elem => {
     elem.addEventListener('click', e => e.preventDefault());
-})
+    elem.addEventListener('mouseover', e => e.target.style.color = randomizeColor());
+});
+
